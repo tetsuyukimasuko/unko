1c83f6fcf2cc85cfb9b27e8125f623b8617c7602
tetsuyukimasuko <35556884+tetsuyukimasuko@users.noreply.github.com>
2018-01-18 19:04:49 +0900

    Update webhook_dialogflow.py

--- webhook_dialogflow.py
+++ webhook_dialogflow.py
@@ -34,33 +34,12 @@ def webhook():
 	elif event_date=='tomorrow':
 		event_date= str(now.year)+"年"+str(now.month)+"月"+str(now.day+1)+"日"
 
-	scope = ['https://www.googleapis.com/auth/drive']
-
-    #ダウンロードしたjsonファイルを同じフォルダに格納して指定する
-	credentials = ServiceAccountCredentials.from_json_keyfile_name('My First Project-fc3744a8d618.json', scope)
-	gc = gspread.authorize(credentials)
-
-    # # 共有設定したスプレッドシートの名前を指定する
-	worksheet = gc.open("Event_Info").sheet1
-	text=""
-
-	try:
-		cell = worksheet.findall(event_date)
-
-
-		for cl in cell:
-			if text!="":
-				text += "また、"+str(worksheet.cell(cl.row,1).value) + "が" + str(worksheet.cell(cl.row,3).value)+"であります。"
-			else:
-				text = str(worksheet.cell(cl.row,1).value) + "が" + str(worksheet.cell(cl.row,3).value)+"であります。"
-
-		r = make_response(jsonify({'speech':text,'displayText':text}))
-		r.headers['Content-Type'] = 'application/json'
-	except:
-		text=event_date+'のイベントは見つかりませんでした。'
+	text =event_date
+	r = make_response(jsonify({'speech':text,'displayText':text}))
+	r.headers['Content-Type'] = 'application/json'
 
 	return r
 
 if __name__ == '__main__':
 	
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0')
